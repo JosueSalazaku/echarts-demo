d670791f3028a4c7bbd2cbd3c68d4717ba55c638
Josué Salazaku <josuesalazaku@gmail.com>
2024-01-31 22:54:03 +0100

    Display side bar chart

--- main.js
+++ main.js
@@ -4,25 +4,32 @@ const myChart = echarts.init(document.getElementById("Chart1"));
 
 // Specify the configuration items and data for the chart
 const option = {
-  title: {
-    text: "ECharts Getting Started Example",
+  dataset: {
+    source: [
+      ["score", "amount", "product"],
+      [89.3, 58212, "Matcha Latte"],
+      [57.1, 78254, "Milk Tea"],
+      [74.4, 41032, "Cheese Cocoa"],
+      [50.1, 12755, "Cheese Brownie"],
+      [89.7, 20145, "Matcha Cocoa"],
+      [68.1, 79146, "Tea"],
+      [19.6, 91852, "Orange Juice"],
+      [10.6, 101852, "Lemon Juice"],
+      [32.7, 20112, "Walnut Brownie"],
+    ],
   },
-  tooltip: {},
-  legend: {
-    data: ["sales"],
-  },
-  xAxis: {
-    data: ["Shirts", "Cardigans", "Chiffons", "Pants", "Heels", "Socks"],
-  },
-  yAxis: {},
+  xAxis: {},
+  yAxis: { type: "category" },
   series: [
     {
-      name: "sales",
       type: "bar",
-      data: [55, 20, 36, 10, 10, 20],
+      encode: {
+        // Map "amount" column to x-axis.
+        x: "amount",
+        // Map "product" row to y-axis.
+        y: "product",
+      },
     },
   ],
 };
-
-// Display the chart using the configuration items and data just specified.
 myChart.setOption(option);
